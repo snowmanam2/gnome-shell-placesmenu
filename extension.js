6ba199fc192caf29df353154df6837495de1690c
Bill Smith <snowmanam2@gmail.com>
2012-05-06 18:26:20 -0500

    Clean up code and remove tabs

--- extension.js
+++ extension.js
@@ -1,3 +1,13 @@
+//vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 encoding=utf-8 textwidth=99
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+// Gnome Shell Places Menu
+// Provides a Places menu in the upper left corner like in Gnome-2
+// 
+// Author:
+//   Bill Smith <snowmanam2@gmail.com>
+//   License: GPLv2+
+//
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
@@ -14,6 +24,7 @@ const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 
+// TODO: Put these in GSettings
 const PLACE_ICON_SIZE = 22;
 const RECENT_NUMBER = 5;
 
@@ -33,8 +44,7 @@ PopupMenuIconItem.prototype = {
         this.addActor(this.label, { align: St.Align.START });
 
         this.icon = icon;
-        if (this.icon != null)
-        {
+        if (this.icon != null) {
             this.icon.style_class = 'popup-menu-icon'; 
             this.addActor(this.icon);
         }
@@ -52,39 +62,37 @@ PopupMenuButtonItem.prototype = {
     _init: function (place, params) {
         PopupMenu.PopupBaseMenuItem.prototype._init.call(this, params);
 
-		this.label = new St.Label({ text: place.name });
+        this.label = new St.Label({ text: place.name });
         this.addActor(this.label, { align: St.Align.START });
 
         this.icon = place.iconFactory(PLACE_ICON_SIZE);
-        if (this.icon != null)
-        {
+        if (this.icon != null) {
             this.icon.style_class = 'popup-menu-icon'; 
             this.addActor(this.icon);
         }
 
-		if (place.isRemovable())
-		{
-			let ejectIcon = new St.Icon({ icon_name: 'media-eject',
+        if (place.isRemovable()) {
+        
+            let ejectIcon = new St.Icon({ icon_name: 'media-eject',
                           style_class: 'hotplug-resident-eject-icon' });
 
-			let button = new St.Button(
-				{ style_class: 'hotplug-resident-eject-button',
-				button_mask: St.ButtonMask.ONE,
-				child: ejectIcon });
+            let button = new St.Button(
+                { style_class: 'hotplug-resident-eject-button',
+                button_mask: St.ButtonMask.ONE,
+                child: ejectIcon });
         
-        	button.connect('clicked', Lang.bind(this, function() {
-            	button.hide();
-            	place.remove();
-        	}));                    
-		
-			this.addActor(button, { align: St.Align.END });
-		}
-	}
-};		
+            button.connect('clicked', Lang.bind(this, function() {
+                button.hide();
+                place.remove();
+            }));                    
+        
+            this.addActor(button, { align: St.Align.END });
+        }
+    }
+};        
 
 
-function _makeLaunchContext(params)
-{
+function _makeLaunchContext(params) {
     params = Params.parse(params, { workspace: -1,
                                     timestamp: 0 });
 
@@ -97,14 +105,12 @@ function _makeLaunchContext(params)
     return launchContext;
 }
 
-function _sortRecentItem(a, b)
-{
-	return Math.max(b.get_modified(), b.get_visited()) - Math.max(a.get_modified(), a.get_visited());
+function _sortRecentItem(a, b) {
+    return Math.max(b.get_modified(), b.get_visited()) - Math.max(a.get_modified(), a.get_visited());
 }
 
-function _mountCallback(src, res, data)
-{
-	src.mount_finish(res,null);
+function _mountCallback(src, res, data) {
+    src.mount_finish(res,null);
 }
 
 PlacesMenuButton.prototype = {
@@ -119,9 +125,9 @@ PlacesMenuButton.prototype = {
         this._label = new Panel.TextShadower();
         this._label.setText('Places');
         bin.set_child(this._label.actor);
-		
-		this._createDefaultPlaces();
-		        
+        
+        this._createDefaultPlaces();
+                
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         this._bookMenu = new PopupMenu.PopupSubMenuMenuItem('Bookmarks');
@@ -129,32 +135,32 @@ PlacesMenuButton.prototype = {
         let bookmarksFile = Gio.file_new_for_path(bookmarksPath);
         this._mon = bookmarksFile.monitor_file(0, null);
         this._mon.connect("changed", 
-        	Lang.bind(this,function(){this._createBookmarks();}));
+            Lang.bind(this,function(){this._createBookmarks();}));
         this._createBookmarks();
         this.menu.addMenuItem(this._bookMenu);
         
         this._volumesMenu = new PopupMenu.PopupSubMenuMenuItem('Volumes');
         this._createVolumes();
         Main.placesManager.connect('mounts-updated',
-        	Lang.bind(this,function(){this._createVolumes();}));
+            Lang.bind(this,function(){this._createVolumes();}));
         this.menu.addMenuItem(this._volumesMenu);
         
         this._mountMenu = new PopupMenu.PopupSubMenuMenuItem('Mounts');
         this._createMounts();
         Main.placesManager.connect('mounts-updated',
-        	Lang.bind(this,function(){this._createMounts();}));
+            Lang.bind(this,function(){this._createMounts();}));
         this.menu.addMenuItem(this._mountMenu);
         
         this._recentManager = Gtk.RecentManager.get_default();
         this._recentList = new PopupMenu.PopupSubMenuMenuItem('Recently Used');
         this._recentManager.connect("changed", 
-        	Lang.bind(this,function(){this._buildRecentList();}));
+            Lang.bind(this,function(){this._buildRecentList();}));
         this._buildRecentList();
         this.menu.addMenuItem(this._recentList);
         
     },
  
- 	_addPlace : function(place, menu) {
+     _addPlace : function(place, menu) {
         
         let icon = place.iconFactory(PLACE_ICON_SIZE);
         let item = new PopupMenuIconItem(place.name,icon);
@@ -162,42 +168,42 @@ PlacesMenuButton.prototype = {
         item.place = place;
         menu.addMenuItem(item);
         item.connect('activate', function(actor,event) {actor.place.launch();});
- 	},
- 	
- 	_addMount : function(place, menu) {
+     },
+     
+     _addMount : function(place, menu) {
         
-		//let icon = place.iconFactory(PLACE_ICON_SIZE);
-		let item = new PopupMenuButtonItem(place);
-		//item.addActor(icon, { align: St.Align.END });
-		item.place = place;
-		menu.addMenuItem(item);
-		item.connect('activate', function(actor,event) {actor.place.launch();});
-	},
+        //let icon = place.iconFactory(PLACE_ICON_SIZE);
+        let item = new PopupMenuButtonItem(place);
+        //item.addActor(icon, { align: St.Align.END });
+        item.place = place;
+        menu.addMenuItem(item);
+        item.connect('activate', function(actor,event) {actor.place.launch();});
+    },
  
- 	_addNoIconPlace : function(place, menu) {
+     _addNoIconPlace : function(place, menu) {
         let item = new PopupMenu.PopupMenuItem(place.name);
         item.place = place;
         menu.addMenuItem(item);
         item.connect('activate', function(){});
- 	},
- 	
- 	_addVolume : function (volume, menu) {
- 	    let gicon = volume.get_icon();
- 	    let icon = St.TextureCache.get_default().load_gicon(null, gicon, PLACE_ICON_SIZE);
- 	    let item = new PopupMenuIconItem (volume.get_drive().get_name() + " : " + volume.get_name(), icon);
- 	    item.volume = volume;
- 	    menu.addMenuItem(item);
- 	    item.connect('activate', Lang.bind(this, function(actor, event) {
- 	        let m = actor.volume.get_mount();
- 	        if (m) {
- 	            let launcher = new PlaceDisplay.PlaceDeviceInfo(m);
+     },
+     
+     _addVolume : function (volume, menu) {
+         let gicon = volume.get_icon();
+         let icon = St.TextureCache.get_default().load_gicon(null, gicon, PLACE_ICON_SIZE);
+         let item = new PopupMenuIconItem (volume.get_drive().get_name() + " : " + volume.get_name(), icon);
+         item.volume = volume;
+         menu.addMenuItem(item);
+         item.connect('activate', Lang.bind(this, function(actor, event) {
+             let m = actor.volume.get_mount();
+             if (m) {
+                 let launcher = new PlaceDisplay.PlaceDeviceInfo(m);
                 launcher.launch();
- 	        }
- 	        else {
- 	            this._mountVolume(actor.volume);
- 	        }
- 	    }));
- 	},
+             }
+             else {
+                 this._mountVolume(actor.volume);
+             }
+         }));
+     },
  
     _createDefaultPlaces : function() {
         this.defaultPlaces = Main.placesManager.getDefaultPlaces();
@@ -207,7 +213,7 @@ PlacesMenuButton.prototype = {
         }
     }, 
     
-    /* The existance of this function may warrant explanation. See, currently only LOCAL uris work, 
+    /* The existance of this function may warrant explanation. See, currently only LOCAL uris work in PlacesManager, 
     so to handle remote locations, we have this reimplementation. Once this is fixed, this nasty function goes away. */
     _getBookmarks : function() {
         let bookmarksPath = GLib.build_filenamev([GLib.get_home_dir(), '.gtk-bookmarks']);
@@ -240,19 +246,17 @@ PlacesMenuButton.prototype = {
             
             if (bookmark == "") continue;
             
-            if (bookmark.substring(0,4) != 'file')
-            {
+            if (bookmark.substring(0,4) != 'file') {
                 let icon = Shell.util_get_icon_for_uri("network:///");
-            	let item = new PlaceDisplay.PlaceInfo('bookmark:' + bookmark, label,
+                let item = new PlaceDisplay.PlaceInfo('bookmark:' + bookmark, label,
                 function(size) {
                     return St.TextureCache.get_default().load_gicon(null, icon, size);
                 },
                 function(params) {
-                    // HACK - we shouldn't depend on nautilus
                     GLib.spawn_command_line_async ('nautilus '+bookmark);
                 });
                 book_places.push(item);
-            	continue;
+                continue;
             }
             
             let icon = Shell.util_get_icon_for_uri(bookmark);
@@ -262,7 +266,7 @@ PlacesMenuButton.prototype = {
                 },
                 function(params) {
                     Gio.app_info_launch_default_for_uri(bookmark, 
-                    	_makeLaunchContext(params));
+                        _makeLaunchContext(params));
                 });
             book_places.push(item);
         }
@@ -272,7 +276,6 @@ PlacesMenuButton.prototype = {
     _createBookmarks : function() {
         this._bookMenu.menu.removeAll();
         
-        //this.bookmarks = Main.placesManager.getBookmarks();
         this.bookmarks = this._getBookmarks();
 
         for (let bookmarkid = 0; bookmarkid < this.bookmarks.length; bookmarkid++) {
@@ -301,8 +304,8 @@ PlacesMenuButton.prototype = {
     },
     
     _createMounts : function() {
-    	this._mountMenu.menu.removeAll();
-		this.mountPlaces = Main.placesManager.getMounts();
+        this._mountMenu.menu.removeAll();
+        this.mountPlaces = Main.placesManager.getMounts();
 
         for (let placeid = 0; placeid < this.mountPlaces.length; placeid++) {
             this._addMount(this.mountPlaces[placeid], this._mountMenu.menu);
@@ -314,18 +317,17 @@ PlacesMenuButton.prototype = {
     
     _buildRecentList: function()
     {
-    	this._recentList.menu.removeAll();
-    	let items = this._recentManager.get_items();
-    	
-    	let displayItems = [];
-    	
-    	items.sort(_sortRecentItem);
-    	for (let i = 0; i < items.length && i < RECENT_NUMBER; i++)
-    	{
-    		let uri = items[i].get_uri();
-    		let label = items[i].get_display_name();
-    	    let icon = Shell.util_get_icon_for_uri(uri);
-    		let item = new PlaceDisplay.PlaceInfo('bookmark:' + uri, label,
+        this._recentList.menu.removeAll();
+        let items = this._recentManager.get_items();
+        
+        let displayItems = [];
+        
+        items.sort(_sortRecentItem);
+        for (let i = 0; i < items.length && i < RECENT_NUMBER; i++) {
+            let uri = items[i].get_uri();
+            let label = items[i].get_display_name();
+            let icon = Shell.util_get_icon_for_uri(uri);
+            let item = new PlaceDisplay.PlaceInfo('bookmark:' + uri, label,
                 function(size) {
                     return St.TextureCache.get_default().load_gicon(null, icon, size);
                 },
@@ -333,39 +335,33 @@ PlacesMenuButton.prototype = {
                     Gio.app_info_launch_default_for_uri(uri, _makeLaunchContext(params));
                 });
             displayItems.push(item);
-    	}
-    	
-    	// The sorted list is reversed, so iterate backwards
-    	for (let i = displayItems.length-1; i >= 0; i--)
-        {
+        }
+        
+        // The sorted list is reversed, so iterate backwards
+        for (let i = displayItems.length-1; i >= 0; i--) {
             this._addPlace(displayItems[i], this._recentList.menu);
         }
-    	
-    	this._recentList.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-    	
+        
+        this._recentList.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        
         let clearitem = new PopupMenu.PopupMenuItem("Clear Recent");
         clearitem.connect("activate", 
-        	Lang.bind(this,function(){this._recentManager.purge_items();}));
-    	this._recentList.menu.addMenuItem(clearitem);
-    	
-    	if (items.length == 0)
-    	{ 
-    	    this._recentList.actor.reactive = false;
-    	    this._recentList.label.style_class = "inactive-label";
-    	    this._recentList._triangle.style_class = "inactive-label"
-    	    //this._recentList.actor.hide();
-    	}
-    	else
-    	{ 
-    	    this._recentList.actor.reactive = true;
-    	    this._recentList.label.style_class = "default-label";
-    	    this._recentList._triangle.style_class = "default-label";
-    	    //this._recentList.actor.show();
+            Lang.bind(this,function(){this._recentManager.purge_items();}));
+        this._recentList.menu.addMenuItem(clearitem);
+        
+        if (items.length == 0) { 
+            this._recentList.actor.reactive = false;
+            this._recentList.label.style_class = "inactive-label";
+            this._recentList._triangle.style_class = "inactive-label"
+        }
+        else { 
+            this._recentList.actor.reactive = true;
+            this._recentList.label.style_class = "default-label";
+            this._recentList._triangle.style_class = "default-label";
         }
     },
     
     _mountVolume: function(volume) {
-        //let operation = new ShellMountOperation.ShellMountOperation(volume);
         volume.mount(0, null, null,
                      Lang.bind(this, this._onVolumeMounted));
     },
@@ -387,17 +383,17 @@ PlacesMenuButton.prototype = {
     },
       
     _launch: function(place) {
-    	GLib.spawn_command_line_async ('nautilus '+place);
+        GLib.spawn_command_line_async ('nautilus '+place);
     },
     
     _connectServer: function() {
-    	GLib.spawn_command_line_async ('nautilus-connect-server');
+        GLib.spawn_command_line_async ('nautilus-connect-server');
     }
 };
 
 function PlacesMenuButton()
 {
-	this._init();
+    this._init();
 }
 
 function init() {
@@ -405,25 +401,20 @@ function init() {
 }
 
 function enable() {
-    /* Remove Application Menu */
-    //restoreState["applicationMenu"] = Main.panel._appMenu.actor;
-    //Main.panel._leftBox.remove_actor(restoreState["applicationMenu"]);  
-
-    /* Place the menu */
+    
     button = new PlacesMenuButton();
     
-    Main.panel._leftBox.add(button.actor);
-    Main.panel._menus.addMenu(button.menu); // Hack to make menu work ...
+    Main.panel._leftBox.add (button.actor);
+    Main.panel._menus.addMenu (button.menu);
+     
 }
 
 function disable() {
-    /* Remove the extension menu */
     
     if (button) {
-        Main.panel._leftBox.remove_actor(button.actor);
+        Main.panel._leftBox.remove_actor (button.actor);
+        Main.panel._menus.removeMenu (button.menu);
         button = null;
     }
-
-    /* Restore Application Menu */
-    //Main.panel._leftBox.add(restoreState["applicationMenu"]);  
+  
 }
